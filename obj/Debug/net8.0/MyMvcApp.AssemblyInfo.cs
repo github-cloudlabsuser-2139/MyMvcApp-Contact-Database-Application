efc1fdb8b96a62bf03c85b631fd369f7f3cbd49c
Your Name <odl_user_1657626@azurehol1164.onmicrosoft.com>
2025-03-26 19:29:41 +0000

    copilot

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e70487b159c3cdfa0a03148d3dcad371c2cb083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce27f123bdf02fa294b457dd66996d4cfa0cce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
